0239bd5146d697c5c55586e4bffa7b6b51f876e1
Basnayake B.Y.N <YasiruDEX@users.noreply.github.com>
2024-07-10 19:11:13 +0530

    update: Simple wall straighning code

--- simulations/custom_bot_test/controllers/custom_robot_controller/custom_robot_controller.py
+++ simulations/custom_bot_test/controllers/custom_robot_controller/custom_robot_controller.py
@@ -20,34 +20,50 @@ def run_robot(robot):
     right_motor.setPosition(float('inf'))
     right_motor.setVelocity(0.0)
 
-    #2 distance sensors ds_left and ds_right
-    # ds_left = robot.getDistanceSensor('ds_left')
-    # ds_left.enable(timestep)
-    # ds_right = robot.getDistanceSensor('ds_right')
-    # ds_right.enable(timestep)
+    #4 distance sensors IR_L1 L2 R1 R2
+    IR_array = []
+    IR_array.append(robot.getDistanceSensor('IR_R2'))
+    IR_array.append(robot.getDistanceSensor('IR_R1'))
+    IR_array.append(robot.getDistanceSensor('IR_L1'))
+    IR_array.append(robot.getDistanceSensor('IR_L2'))
+    for i in range(4):
+        IR_array[i].enable(timestep)
         
     # Main loop:
     # - perform simulation steps until Webots is stopping the controller
     while robot.step(timestep) != -1:
         # Read the sensors:
-        # left_sensor_value = ds_left.getValue()
-        # right_sensor_value = ds_right.getValue()
+        IR_values = []
+        for i in range(4):
+            IR_values.append(IR_array[i].getValue())
     
         # Process sensor data here.
-        # left_wall = left_sensor_value > 80
-        # right_wall = right_sensor_value > 80
+        #ir value 500 is the 0 speed if decrease motor speed negative else positive linear velocity
+        right_speed = (IR_values[0] - 500)/10
+        left_speed = (IR_values[3] - 500)/10
 
+        #limit speeds to -10 and +10
+        if right_speed > 10:
+            right_speed = 10
+        elif right_speed < -10:
+            right_speed = -10
+        if left_speed > 10:
+            left_speed = 10
+        elif left_speed < -10:
+            left_speed = -10
 
-        left_speed = max_speed
-        right_speed = max_speed
+        # left_speed = max_speed
+        # right_speed = max_speed
 
     
         # Enter here functions to send actuator commands, like:
+        #forward if speeds are positive else reverse
         left_motor.setVelocity(left_speed)
         right_motor.setVelocity(right_speed)
 
-        print('left_speed: ' + str(left_speed) + ' right_speed: ' + str(right_speed))
 
+        print('left_speed: ' + str(left_speed) + ' right_speed: ' + str(right_speed))
+        print('IR_values: ' + str(IR_values))
 # Enter here exit cleanup code.
 
 if __name__ == "__main__":
