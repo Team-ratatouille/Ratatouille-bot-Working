613a62df3e913eaca53f1eb2b4c7adbbae4a2506
Basnayake B.Y.N <131932017+YasiruDEX@users.noreply.github.com>
2024-08-21 10:16:02 +0530

    update: Maze solver flood fill iteration 1

--- simulations/custom_bot_test/controllers/custom_robot_controller/custom_robot_controller.py
+++ simulations/custom_bot_test/controllers/custom_robot_controller/custom_robot_controller.py
@@ -3,6 +3,20 @@
 from controller import Robot
 import time
 
+# 31x31 grid of -1 s
+maze_discovered = [[-1 for i in range(31)] for j in range(31)]
+location = [30, 0]
+maze_discovered[location[0]][location[1]] = 0
+heading = 0 # 0: North, 1: East, 2: South, 3: West
+
+#save the grid in a txt file
+def save_maze():
+    global maze_discovered
+    with open('maze_discovered.txt', 'w') as f:
+        for row in maze_discovered:
+            f.write(' '.join([str(elem) for elem in row]))
+            f.write('\n')
+
 def update_encoder_init_values(left_motor_encoder, right_motor_encoder):
     left_encoder_init = left_motor_encoder.getValue()
     right_encoder_init = right_motor_encoder.getValue()
@@ -13,31 +27,102 @@ def stop_robot(left_motor, right_motor):
     right_motor.setVelocity(0)
 
 def check_aval(IR_values, traverse=[]):
+    global maze_discovered
+    global location
+    global heading
+
     left_aval = False
     right_aval = False
+    front_aval = False
+
     if IR_values[2] == 1000:
         left_aval = True
     if IR_values[1] == 1000:
         right_aval = True
+    if IR_values[0] >= 800 and IR_values[3] >= 800:
+        front_aval = True
 
-    if left_aval and right_aval:
-        print('left and right available')
-        traverse.append("left")
-        traverse.append("forward")
-    elif left_aval:
+    if left_aval:
         print('left available')
+    if right_aval:
+        print('right available')
+    if front_aval:
+        print('front available')
+
+    
+    if front_aval:
+        traverse.append("forward")
+        print('forward')
+    elif left_aval:
         traverse.append("left")
         traverse.append("forward")
+        print('left')
     elif right_aval:
-        print('right available')
         traverse.append("right")
         traverse.append("forward")
-    else:
-        traverse.append("stop")
+        print('right')
+
+    #update the discovered maze with the findings
+    if heading == 0:
+        if left_aval:
+            if location[1] - 2 >= 0:
+                maze_discovered[location[0]][location[1] - 1] = 0
+                maze_discovered[location[0]][location[1] - 2] = 0
+        if right_aval:
+            if location[1] + 2 <= 30:
+                maze_discovered[location[0]][location[1] + 1] = 0
+                maze_discovered[location[0]][location[1] + 2] = 0
+        if front_aval:
+            if location[0] - 2 >= 0:
+                maze_discovered[location[0] - 1][location[1]] = 0
+                maze_discovered[location[0] - 2][location[1]] = 0
+    elif heading == 1:
+        if left_aval:
+            if location[0] - 2 >= 0:
+                maze_discovered[location[0] - 1][location[1]] = 0
+                maze_discovered[location[0] - 2][location[1]] = 0
+        if right_aval:
+            if location[0] + 2 <= 30:
+                maze_discovered[location[0] + 1][location[1]] = 0
+                maze_discovered[location[0] + 2][location[1]] = 0
+        if front_aval:
+            if location[1] + 2 <= 30:
+                maze_discovered[location[0]][location[1] + 1] = 0
+                maze_discovered[location[0]][location[1] + 2] = 0
+    elif heading == 2:
+        if left_aval:
+            if location[1] + 2 <= 30:
+                maze_discovered[location[0]][location[1] + 1] = 0
+                maze_discovered[location[0]][location[1] + 2] = 0
+        if right_aval:
+            if location[1] - 2 >= 0:
+                maze_discovered[location[0]][location[1] - 1] = 0
+                maze_discovered[location[0]][location[1] - 2] = 0
+        if front_aval:
+            if location[0] + 2 <= 30:
+                maze_discovered[location[0] + 1][location[1]] = 0
+                maze_discovered[location[0] + 2][location[1]] = 0
+    elif heading == 3:
+        if left_aval:
+            if location[0] + 2 <= 30:
+                maze_discovered[location[0] + 1][location[1]] = 0
+                maze_discovered[location[0] + 2][location[1]] = 0
+        if right_aval:
+            if location[0] - 2 >= 0:
+                maze_discovered[location[0] - 1][location[1]] = 0
+                maze_discovered[location[0] - 2][location[1]] = 0
+        if front_aval:
+            if location[1] - 2 >= 0:
+                maze_discovered[location[0]][location[1] - 1] = 0
+                maze_discovered[location[0]][location[1] - 2] = 0
 
     return traverse
 
 def run_robot(robot):
+    global maze_discovered
+    global location
+    global heading
+
     # get the time step of the current world.
     timestep = int(robot.getBasicTimeStep())
     max_speed = 6.28
@@ -79,7 +164,6 @@ def run_robot(robot):
     # print('left_encoder_init: ' + str(left_encoder_init) + ' right_encoder_init: ' + str(right_encoder_init))
 
     # mode of operation
-    mode = "right"
     traverse = ["forward"]
     index = 0
     command_count = 1
@@ -87,7 +171,7 @@ def run_robot(robot):
     speed_factor = 2
     P = 0.25
 
-    cell_size = 100 #12.8
+    cell_size = 12.8
     turn_size = 25
 
     wall_triggerd = False
@@ -113,7 +197,14 @@ def run_robot(robot):
     ###########################################################################################
     while robot.step(timestep) != -1:
         # set mode of operation
-        mode = traverse[index]
+        try:
+            mode = traverse[index]
+        except:
+            left_speed = 0
+            right_speed = 0
+            save_maze()
+            print('Map saved')
+            break
 
         # Read the IR sensors:
         IR_values = []
@@ -122,30 +213,30 @@ def run_robot(robot):
             IR_values.append(round(IR_array[i].getValue(), 2))
 
         # if front distance sensors triggered next mode
-        if IR_values[0] < 750 and IR_values[3] < 750:
-            if not wall_triggerd:
+        # if IR_values[0] < 750 and IR_values[3] < 750:
+        #     if not wall_triggerd:
 
-                #check if wall is available
-                traverse = check_aval(IR_values, traverse)
+        #         #check if wall is available
+        #         traverse = check_aval(IR_values, traverse)
 
-                wall_triggerd = True
-                index += 1
-                mode = traverse[index]
+        #         wall_triggerd = True
+        #         index += 1
+        #         mode = traverse[index]
 
-                #reset encoder values
-                encoder_init = False
-                command_count = 1
-                print('wall triggerd mode: ' + mode)
-        else:
-            wall_triggerd = False
+        #         #reset encoder values
+        #         encoder_init = False
+        #         command_count = 1
+        #         print('wall triggerd mode: ' + mode)
+        # else:
+        #     wall_triggerd = False
 
         # Read if the command array has the same command consecutively
-        while True:
-            if index + 1 < len(traverse) and traverse[index] == traverse[index + 1]:
-                command_count += 1
-                index += 1
-            else:
-                break
+        # while True:
+        #     if index + 1 < len(traverse) and traverse[index] == traverse[index + 1]:
+        #         command_count += 1
+        #         index += 1
+        #     else:
+        #         break
 
         # Read the gyro sensors:
         gyro_values = []
@@ -161,22 +252,6 @@ def run_robot(robot):
         # Read encoders with 2 decimal points
         left_encoder = round(left_motor_encoder.getValue(), 2) - left_encoder_init
         right_encoder = round(right_motor_encoder.getValue(), 2) - right_encoder_init
-    
-        # Process sensor data here.
-        #ir value 500 is the 0 speed if decrease motor speed negative else positive linear velocity
-        # right_speed = (IR_values[0] - 500)/10
-        # left_speed = (IR_values[3] - 500)/10
-
-        # stop when counted 10 rotations
-        # if left_encoder > 13.6:
-        #     left_speed = 0
-        # else:
-        #     left_speed = 3
-
-        # if right_encoder > 13.6:
-        #     right_speed = 0
-        # else:
-        #     right_speed = 3
 
         #integrate gyro reading to get the angle (yawn)
         if (time.time() - integral_start) > 0.05:
@@ -199,6 +274,9 @@ def run_robot(robot):
                 #reset encoder values
                 encoder_init = False
                 command_count = 1
+
+                #update the heading
+                heading = (heading + 1) % 4
                         
         elif mode == "left":
             # rotate 90 degrees left
@@ -214,6 +292,9 @@ def run_robot(robot):
                 #reset encoder values
                 encoder_init = False
                 command_count = 1
+
+                #update the heading
+                heading = (heading - 1) % 4
 
         elif mode == "stop":
             left_speed = 0
@@ -231,40 +312,44 @@ def run_robot(robot):
             else:
                 right_speed = cell_size * command_count - right_encoder + damp_speed
 
-            # if angle > 0:
-            #     left_speed = left_speed + angle/3
-            # else:
-            #     right_speed = right_speed + angle/3*(-1)
-
-
             if right_encoder > cell_size * command_count and left_encoder > cell_size * command_count:
 
                 left_speed = 0
                 right_speed = 0
                 angle = 0
-                # time.sleep(1)
                 index += 1
 
                 #reset encoder values
                 encoder_init = False
                 command_count = 1
 
+                #update the location with heading and forward move
+                if heading == 0:
+                    maze_discovered[location[0]-1][location[1]] = 0
+                    location[0] -= 2
+                elif heading == 1:
+                    maze_discovered[location[0]][location[1]+1] = 0
+                    location[1] += 2
+                elif heading == 2:
+                    maze_discovered[location[0]+1][location[1]] = 0
+                    location[0] += 2
+                elif heading == 3:
+                    maze_discovered[location[0]][location[1]-1] = 0
+                    location[1] -= 2
+
+                #update the heading
+                maze_discovered[location[0]][location[1]] = 0
+
                 #check if wall is available
-                # check_aval(IR_values)
+                traverse = check_aval(IR_values, traverse)
 
         # avoid hitting the walls
-        if IR_values[1] < 300:
-            print('right')
-            left_speed = left_speed - 1
-        if IR_values[2] < 300:
-            print('left')
-            right_speed = right_speed - 1
-                
-                
-        # rotate 90 degrees left
-        # while left_encoder < 4.46:
-        #     left_speed = -3
-        #     right_speed = 3
+        # if IR_values[1] < 300:
+        #     print('right')
+        #     left_speed = left_speed - 1
+        # if IR_values[2] < 300:
+        #     print('left')
+        #     right_speed = right_speed - 1
 
         #limit speeds to -10 and +10
         try:
@@ -283,12 +368,6 @@ def run_robot(robot):
 
             left_motor.setVelocity(left_speed)
             right_motor.setVelocity(right_speed)
-
-            # print('left_speed: ' + str(left_speed) + ' right_speed: ' + str(right_speed))
-            # print('IR_values: ' + str(IR_values))
-            # print ('left_encoder: ' + str(left_encoder) + ' right_encoder: ' + str(right_encoder))
-            # print all these in same line
-            # print('left_speed: ' + str(left_speed) + ' right_speed: ' + str(right_speed) + ' IR_values: ' + str(IR_values) + ' left_encoder: ' + str(left_encoder) + ' right_encoder: ' + str(right_encoder))
 
             #set speeds to 0
             left_speed = 0
